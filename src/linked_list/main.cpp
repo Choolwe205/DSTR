ad6c8ccbb69ea660138e4fd8ca033de91396ea6b
Choolwe205 <110763821+Choolwe205@users.noreply.github.com>
2026-04-13 16:16:21 +0800

    Changed to classes

--- src/linked_list/main.cpp
+++ src/linked_list/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct resident{
+class resident{
     public:
     string residentID;
     int age;
@@ -10,10 +10,20 @@ struct resident{
     double carbonEmissions;
     int averageDayPerMonth;
     resident* next;
-    
-};
-void traversePrint(resident *head){
-    resident* current=head;
+
+    resident(){};
+
+    resident(string residentID,int age,string modeOfTransport, int dailyDistance, double carbonEmissions,int averageDayPerMonth,resident* next){
+    this->residentID= residentID;
+    this->age= age;
+    this->modeOfTransport= modeOfTransport;
+    this->dailyDistance= dailyDistance;
+    this->carbonEmissions= carbonEmissions;
+    this->averageDayPerMonth= averageDayPerMonth;
+    this->next=next;
+    }
+    void traversePrint(){
+    resident* current=this;
     while (current!=nullptr)
     {
         cout<<current->residentID<<", ";
@@ -25,16 +35,19 @@ void traversePrint(resident *head){
         current=current->next;
     }
     
-}
+    };
+    
+};
 int main(){
 
     cout<<"Linked Lists version for demo\n";
-    resident *head=new resident{"A001",34,"car",15,0.21,25,nullptr};
-    resident *second=new resident{"A002",42,"bus",12,0.05,26,nullptr};
-    resident *third=new resident{"A003",28,"bicycle",8,0,26,nullptr};
+    resident *head=new resident("A001",34,"car",15,0.21,25,nullptr);
+    resident *second=new resident("A002",42,"bus",12,0.05,26,nullptr);
+    resident *third=new resident("A003",28,"bicycle",8,0,26,nullptr);
     head->next=second;
     second->next=third;
-    traversePrint(head);
+
+    head->traversePrint();
 
     return 0;
     
