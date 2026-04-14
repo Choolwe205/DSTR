0067ff317e9241b7dc9854b608f789e923812f9c
Choolwe205 <110763821+Choolwe205@users.noreply.github.com>
2026-04-14 16:49:21 +0800

    Insertion added for understanding

--- src/linked_list/main.cpp
+++ src/linked_list/main.cpp
@@ -22,7 +22,7 @@ class resident{
     this->averageDayPerMonth= averageDayPerMonth;
     this->next=next;
     }
-    void traversePrint(){
+    void traversePrint(resident*head){
     resident* current=this;
     while (current!=nullptr)
     {
@@ -34,12 +34,12 @@ class resident{
         cout<<current->averageDayPerMonth<<endl;
         current=current->next;
     }
-    cout<<"null";
+    cout<<"End";
     };
-    int getSize()
+    int getSize(resident*head)
     {
         int count=0;
-        resident *current=this;
+        resident *current=head;
         while( current !=nullptr)
         {
             count++;
@@ -47,8 +47,27 @@ class resident{
         }
         return count;
     };
-    
-};
+    void insertAtStart(string ID,int resage,string resmodeOfTransport, int resdailyDistance, double rescarbonEmissions,int resaverageDayPerMonth,resident* &head){
+        resident*newResident=new resident{ID,resage,resmodeOfTransport,resdailyDistance,rescarbonEmissions,resaverageDayPerMonth,nullptr};
+        newResident->next=head;
+        head=newResident;
+    };
+
+    void insertAtEnd(string ID,int resage,string resmodeOfTransport, int resdailyDistance, double rescarbonEmissions,int resaverageDayPerMonth,resident* &head)
+    {
+        resident*newResident=new resident{ID,resage,resmodeOfTransport,resdailyDistance,rescarbonEmissions,resaverageDayPerMonth,nullptr};
+        if (head==nullptr){
+        head=newResident;
+        return;
+    };
+        resident*currentResident=head;
+        while(currentResident->next!=nullptr)
+        {
+            currentResident=currentResident->next;
+        };
+        currentResident->next=newResident;
+    };
+    };  
 int main(){
 
     cout<<"Linked Lists version for demo\n";
@@ -57,9 +76,10 @@ int main(){
     resident *third=new resident("A003",28,"bicycle",8,0,26,nullptr);
     head->next=second;
     second->next=third;
-    head->traversePrint();
-    cout<< "The size is: "<<head->getSize();
-
+    // head->traversePrint();
+    cout<< "The size is: "<<head->getSize(head)<<endl;
+    head->insertAtStart("A004",26,"car",8,0,26,head);
+    head->traversePrint(head);
     return 0;
     
 }
