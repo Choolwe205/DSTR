a8526f75ca92282bbfa3f2aec87ff3e1c79fb5aa
Choolwe205 <110763821+Choolwe205@users.noreply.github.com>
2026-04-22 16:02:43 +0800

    Space che

--- src/linked_list/main.cpp
+++ src/linked_list/main.cpp
@@ -15,7 +15,6 @@ class resident{
     resident* next;
 
     resident(){};
-
     resident(string residentID,int age,string modeOfTransport, int dailyDistance, double carbonEmissionFactor,int averageDayPerMonth,resident* next){
     this->residentID= residentID;
     this->age= age;
